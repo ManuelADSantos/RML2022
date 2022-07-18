79072ce686f25b33b4fda6c7038728c9b2edc181
ManuelADSantos <manel.a.d.santos@gmail.com>
2022-07-18 11:53:02 +0100

    Update bot_RML2022.h

--- biblioteca/RML2022/bot_RML2022.h
+++ biblioteca/RML2022/bot_RML2022.h
@@ -59,12 +59,12 @@
 
 //----------------------------------------------------------
 // Auxiliry Libraries
-#include "Wire.h"
+#include <Wire.h>
 //----------------------------------------------------------
 // External Auxiliry Libraries
 #include "Adafruit_NeoPixel.h"
 #include "Adafruit_TCS34725.h"
-#include "L53L0X.h"
+#include "VL53L0X.h"
 //----------------------------------------------------------
 
 class bot_RML2022
